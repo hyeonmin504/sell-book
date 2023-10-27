923bd3e77f626e48bb1be1aaa53243f60619102b
hyeonmin <hyunmin504@naver.com>
2023-10-28 04:05:24 +0900

    sell_page

--- app/src/main/java/com/example/booksell/SellPage.java
+++ app/src/main/java/com/example/booksell/SellPage.java
@@ -7,11 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 
 public class SellPage extends AppCompatActivity {
     Button btn_my, btn_chat,btn_sell,btn_buy;
     EditText edt_bookname,edt_writer,edt_publisher,edt_publisher_date,edt_price;
-
+    private final String TAG = this.getClass().getSimpleName();
+    ImageView imageview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +40,33 @@ public class SellPage extends AppCompatActivity {
         edt_publisher = findViewById(R.id.edt_publisher);
         edt_publisher_date = findViewById(R.id.edt_publisher_date);
         edt_price = findViewById(R.id.edt_price);
+
+        imageview = findViewById(R.id.iv_book);
+        ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),new ActivityResultCallback<ActivityResult>(){
+                    @Override
+                    public void onActivityResult(ActivityResult result)
+                    {
+                        if (result.getResultCode() == RESULT_OK)
+                        {
+                            Log.e(TAG, "result : " + result);
+                            Intent intent = result.getData();
+                            Log.e(TAG, "intent : " + intent);
+                            Uri uri = intent.getData();
+                            Log.e(TAG, "uri : " + uri);
+//                        imageview.setImageURI(uri);
+                            Glide.with(SellPage.this)
+                                    .load(uri)
+                                    .into(imageview);
+                        }
+                    }
+                });
+
+        imageview.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            launcher.launch(intent);
+        });
 
         btn_buy.setOnClickListener(new View.OnClickListener() {
             @Override
