fb200f73680f2001e5792d8d7fe8ab0f8ba50643
hyeonmin <hyunmin504@naver.com>
2023-10-27 16:19:23 +0900

    first commit

---
+++ app/src/main/java/com/example/booksell/BookInfoPage.java
@@ -0,0 +1,14 @@
+package com.example.booksell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookInfoPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_info_page);
+    }
+}
---
+++ app/src/main/java/com/example/booksell/ChatListPage.java
@@ -0,0 +1,14 @@
+package com.example.booksell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatListPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_list_page);
+    }
+}
