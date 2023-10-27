e67fb2edce2b9eda2a43669dab98159839bf1733
hyeonmin <hyunmin504@naver.com>
2023-10-27 20:07:49 +0900

    booksell

--- app/src/main/java/com/example/booksell/SellPage.java
+++ app/src/main/java/com/example/booksell/SellPage.java
@@ -2,13 +2,61 @@ package com.example.booksell;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SellPage extends AppCompatActivity {
+    Button btn_my, btn_chat,btn_sell,btn_buy;
+    EditText edt_bookname,edt_writer,edt_publisher,edt_publisher_date,edt_price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell_page);
+
+        btn_my = findViewById(R.id.btn_my);
+        btn_chat = findViewById(R.id.btn_chat);
+        btn_sell = findViewById(R.id.btn_sell);
+        btn_buy = findViewById(R.id.btn_buy);
+        edt_bookname = findViewById(R.id.edt_bookname);
+        edt_writer = findViewById(R.id.edt_writer);
+        edt_publisher = findViewById(R.id.edt_publisher);
+        edt_publisher_date = findViewById(R.id.edt_publisher_date);
+        edt_price = findViewById(R.id.edt_price);
+
+        btn_buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SellPage.this,BuyPage.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_sell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SellPage.this,SellPage.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SellPage.this,ChatListPage.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_my.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SellPage.this,MyPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
