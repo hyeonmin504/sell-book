f765ee1e525da88cc757cec29179179e2874e9ae
Jaehyeon Kim <syukjh0314@naver.com>
2023-10-28 02:58:08 +0900

    채팅 리스트 틀 작성

--- app/src/main/java/com/example/booksell/ChatListPage.java
+++ app/src/main/java/com/example/booksell/ChatListPage.java
@@ -2,13 +2,54 @@ package com.example.booksell;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChatListPage extends AppCompatActivity {
+
+    Button btn_my, btn_chat,btn_sell,btn_buy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_list_page);
+
+        btn_my = findViewById(R.id.btn_my);
+        btn_chat = findViewById(R.id.btn_chat);
+        btn_sell = findViewById(R.id.btn_sell);
+        btn_buy = findViewById(R.id.btn_buy);
+        btn_buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChatListPage.this,BuyPage.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_sell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChatListPage.this,SellPage.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChatListPage.this,ChatListPage.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_my.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChatListPage.this,MyPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
