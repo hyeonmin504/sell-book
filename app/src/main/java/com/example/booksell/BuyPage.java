00bb4d8609a47dac845c56fc68f14c106fa8a0e5
Jaehyeon Kim <syukjh0314@naver.com>
2023-10-28 03:12:37 +0900

    검색 페이지 버튼 구현

--- app/src/main/java/com/example/booksell/BuyPage.java
+++ app/src/main/java/com/example/booksell/BuyPage.java
@@ -2,13 +2,54 @@ package com.example.booksell;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BuyPage extends AppCompatActivity {
+
+    Button btn_my, btn_chat,btn_sell,btn_buy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_page);
+
+        btn_my = findViewById(R.id.btn_my);
+        btn_chat = findViewById(R.id.btn_chat);
+        btn_sell = findViewById(R.id.btn_sell);
+        btn_buy = findViewById(R.id.btn_buy);
+        btn_buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BuyPage.this,BuyPage.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_sell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BuyPage.this,SellPage.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BuyPage.this,ChatListPage.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_my.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BuyPage.this,MyPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
