fd1250d29d67a49427f8b8073c4acc65a3b960d9
hyeonmin <hyunmin504@naver.com>
2023-10-30 22:33:29 +0900

    mainActivity_update

---
+++ app/src/main/java/com/example/booksell/ImageSliderAdapter.java
@@ -0,0 +1,42 @@
+package com.example.booksell;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class ImageSliderAdapter extends PagerAdapter {
+    private Context context;
+    private int[] images = {R.drawable.image1, R.drawable.image2, R.drawable.image4,R.drawable.image5,R.drawable.image6};
+
+    public ImageSliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.image_slider_item, container, false);
+        ImageView imageView = view.findViewById(R.id.imageView);
+        imageView.setImageResource(images[position]);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+}
--- app/src/main/java/com/example/booksell/MainActivity.java
+++ app/src/main/java/com/example/booksell/MainActivity.java
@@ -1,54 +1,64 @@
 package com.example.booksell;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
-import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn_my, btn_chat,btn_sell,btn_buy;
-
+    private ViewPager viewPager;
+    private ImageSliderAdapter adapter;
+    private int currentPage = 0;
+    private Handler handler;
+    private Runnable runnable;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_my = findViewById(R.id.btn_my);
-        btn_chat = findViewById(R.id.btn_chat);
-        btn_sell = findViewById(R.id.btn_sell);
-        btn_buy = findViewById(R.id.btn_buy);
-        btn_buy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,BuyPage.class);
-                startActivity(intent);
-            }
-        });
 
-        btn_sell.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,BookAdapter.class);
-                startActivity(intent);
-            }
-        });
+        viewPager = findViewById(R.id.viewPager);
+        adapter = new ImageSliderAdapter(this);
+        viewPager.setAdapter(adapter);
+        textView = findViewById(R.id.textView);
 
-        btn_chat.setOnClickListener(new View.OnClickListener() {
+        textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,ChatListPage.class);
+                // 클릭 이벤트 발생 시, 다른 액티비티로 전환
+                Intent intent = new Intent(MainActivity.this, BuyPage.class);
                 startActivity(intent);
             }
         });
+        handler = new Handler(Looper.getMainLooper());
 
-        btn_my.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,MyPage.class);
-                startActivity(intent);
+        // 타이머를 사용하여 이미지 자동 전환
+        runnable = new Runnable() {
+            public void run() {
+                if (currentPage == adapter.getCount() - 1) {
+                    currentPage = 0;
+                }
+                viewPager.setCurrentItem(currentPage++, true);
+                handler.postDelayed(this, 2500); // 2.5초마다 전환
             }
-        });
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handler.postDelayed(runnable, 3000); // 액티비티가 활성화되면 타이머 시작
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        handler.removeCallbacks(runnable); // 액티비티가 비활성화되면 타이머 중지
     }
 }
