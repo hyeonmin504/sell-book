e1695445e95859547afd8d9cd9eac1dc714763a0
hyunmin504 <hyunmin504@naver.com>
2023-11-28 17:15:14 +0900

    chatListPage_buyer->price

--- app/src/main/java/com/example/booksell/ChatRoomAdapter.java
+++ app/src/main/java/com/example/booksell/ChatRoomAdapter.java
@@ -10,6 +10,10 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.List;
 
 public class ChatRoomAdapter extends RecyclerView.Adapter<ChatRoomAdapter.ChatRoomViewHolder> {
@@ -44,20 +48,22 @@ public class ChatRoomAdapter extends RecyclerView.Adapter<ChatRoomAdapter.ChatRo
 
         private TextView sellerTextView;
         private TextView bookNameTextView;
-        private TextView buyerTextView;
+        private TextView priceTextView;
 
         public ChatRoomViewHolder(@NonNull View itemView) {
             super(itemView);
 
             sellerTextView = itemView.findViewById(R.id.sellerTextView);
             bookNameTextView = itemView.findViewById(R.id.bookNameTextView);
-            buyerTextView = itemView.findViewById(R.id.buyerTextView);
+            priceTextView = itemView.findViewById(R.id.priceTextView);
         }
 
         public void bind(ChatRoom chatRoom) {
             sellerTextView.setText("판매자: " + chatRoom.getSeller());
             bookNameTextView.setText("도서명: " + chatRoom.getBookName());
-            buyerTextView.setText("구매자: " + chatRoom.getBuyer());
+
+            // 가격 가져와서 TextView에 설정
+            fetchPrice(chatRoom.getSeller(), chatRoom.getBookName(), priceTextView);
 
             // 클릭 이벤트 추가 (채팅방으로 이동)
             itemView.setOnClickListener(v -> {
@@ -68,6 +74,39 @@ public class ChatRoomAdapter extends RecyclerView.Adapter<ChatRoomAdapter.ChatRo
                 intent.putExtra("buyer", chatRoom.getBuyer());
                 context.startActivity(intent);
             });
+        }
+
+        // 가격 가져와서 TextView에 설정
+        private void fetchPrice(String seller, String bookName, TextView priceTextView) {
+            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+            CollectionReference bookInfoCollection = firestore.collection("bookInfo");
+
+            // bookInfo 컬렉션에서 해당 판매자와 도서명에 맞는 가격을 가져옴
+            bookInfoCollection.whereEqualTo("email", seller)
+                    .whereEqualTo("bookName", bookName)
+                    .get()
+                    .addOnSuccessListener(queryDocumentSnapshots -> {
+                        if (!queryDocumentSnapshots.isEmpty()) {
+                            // 쿼리 결과에서 가격을 가져옴
+                            DocumentSnapshot documentSnapshot = queryDocumentSnapshots.getDocuments().get(0);
+                            Long priceLong = documentSnapshot.getLong("bookPrice");
+
+                            if (priceLong != null) {
+                                int price = priceLong.intValue(); // long으로도 변경 가능
+                                priceTextView.setText("가격: " + price + "원");
+                            } else {
+                                // 해당하는 도서 정보가 없는 경우
+                                priceTextView.setText("가격 정보 없음");
+                            }
+                        } else {
+                            // 해당하는 도서 정보가 없는 경우
+                            priceTextView.setText("가격 정보 없음");
+                        }
+                    })
+                    .addOnFailureListener(e -> {
+                        // 쿼리 실패한 경우
+                        priceTextView.setText("가격 정보를 가져오지 못했습니다.");
+                    });
         }
     }
 }
